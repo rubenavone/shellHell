25fb7989c488b5bdc9369141c15e11c0a6f12ab1
rubenavone <navone_ruben@msn.com>
2019-03-14 20:26:04 +0100

    ajout d'un autre test

---
+++ essai.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+
+echo "ceci est mon premier essaie" 
+
+
+#Ceci est une ligne de commentaire 
+#a noter que le suffixe .sh n'est pas obligatoire mais permet d'obtenir la colorisation dans les
+#de texte 
